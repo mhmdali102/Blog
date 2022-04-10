19a16c7a3c93193d7b7333d921b52b4ef2378f18
mhmdali102 <mhmdali102@bk.ru>
2022-04-10 15:39:12 +0300

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
+    title: 'Mhmd Ali Hsen Portfolio',
+    description: 'Mhmd Ali Hsen | Front End Web Developer',
     image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
   }
 
--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Mhmd Ali Hsen',
+    site_url: 'https://mhmdali102.vercel.app',
+    feed_url: 'https://mhmdali102.vercel.app/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
